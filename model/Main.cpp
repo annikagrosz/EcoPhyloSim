a571acb69a73eda5dfbeb5ac0fbc8e71a15cd794
Betim Musa <musab@informatik.uni-freiburg.de>
2015-08-04 14:05:17 +0200

    changed main

--- model/Main.cpp
+++ model/Main.cpp
@@ -16,7 +16,6 @@
 
 
 
-<<<<<<< HEAD
 int main() {
    unsigned int runs = 3000; // number of Generations
    bool neutral = true; // neutral model or not
@@ -34,57 +33,11 @@ int main() {
    // Just to test
    Parameters* pa = new Parameters();
    std::cout << "Value of numberOfRuns: " << pa->getParameterValue<int>(std::string("numberOfRuns")) << std::endl;
-=======
-int main(){
-	unsigned int runs = 10000; // number of Generations
-	bool neutral = false; // neutral model or not
-	bool dd = true; // Density dependent or independent model
-	bool env = false; // environmentally dependent or independent model
-	bool mort = true; // mortality fitness
-	bool repro = false; // reproductive fitness
-	int dispersal = 3; // 1 = global dispersal, 2 = nearest neighbor dispersal, 3= kernel dispersal
-	int xDim = 100; // Number of grid cells in x-direction
-	int yDim = 100; // number of grid cells in y-direction
-//	srand(1500);
-	double specrate = 1.0;
-	int densityCut = 2;
-	int dispersalCut = 2;
-	int mortalityStrength = 10;
-	std:: string saveLoc = "C:/Users/Paul/Documents/GitHub/phylogeography/results/saveLocation";
-	RandomGen ran;
-	ran.seedrand(1000);
-	// Just to test
-	Parameters* pa = new Parameters();
-	std::cout << "Value of numberOfRuns: " << pa->getParameterValue<int>(std::string("numberOfRuns")) << std::endl;
->>>>>>> 76a48d936766d2e72147ee9f9eeeb33cc0c0e917
 
    // avoid inconsistent input
    if(neutral && dd) throw std::runtime_error("A neutral model can't be density dependent!");
    if(neutral && env) throw std::runtime_error("A neutral model can't be depending on the environment!");
 
-<<<<<<< HEAD
-=======
-	//Running the model
-	model Model(xDim,yDim,dispersal, neutral, dd, env, mort, repro, runs, specrate, dispersalCut, densityCut, mortalityStrength, saveLoc);
-	Model.update(runs, saveLoc);
-//	Model.get_data();
-//	Model.getclimate();
-//	Model.gettraits();
-	if(dispersal ==1)
-	{
-	Model.m_Global->m_Phylogeny.prunePhylogeny(Model.m_Global->m_Phylogeny.m_FullPhylogeny);
-	Model.m_Global->m_Phylogeny.writePhylogeny(1, runs, Model.m_Global->m_Phylogeny.m_PrunedPhylo,'P');
-	Model.m_Global->m_Phylogeny.writePhylogeny(1, runs, Model.m_Global->m_Phylogeny.m_FullPhylogeny,'F');
-	Model.m_Global->m_Phylogeny.writeSpeciesData();
-	}
-	else
-	{
-	Model.m_Local->m_Phylogeny.prunePhylogeny(Model.m_Local->m_Phylogeny.m_FullPhylogeny);
-	Model.m_Local->m_Phylogeny.writePhylogeny(1, runs, Model.m_Local->m_Phylogeny.m_PrunedPhylo,'P');
-	Model.m_Local->m_Phylogeny.writePhylogeny(1, runs, Model.m_Local->m_Phylogeny.m_FullPhylogeny,'F');
-	Model.m_Local->m_Phylogeny.writeSpeciesData();
-	}
->>>>>>> 76a48d936766d2e72147ee9f9eeeb33cc0c0e917
 
    //Running the model
    PhylSimModel Model(xDim,yDim,dispersal, neutral, dd, env, runs, specrate, dispersalCut, densityCut);
