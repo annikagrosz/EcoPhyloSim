340b1bedd591a92f2a8465930a1a3cb49182f935
Florian Hartig <florianhartig@users.noreply.github.com>
2015-08-17 13:47:08 +0200

    Reorganized function for community metrics

--- PhylGeo/R/communityMetrics.R
+++ PhylGeo/R/communityMetrics.R
@@ -1,3 +1,12 @@
+#' @title Species richness
+#' @description Calculate the total species richness of a community
+#' @param matrix A square matrix containing a species community (one individual per grid cell) 
+#' @return An integer value for the species richness
+specRich <- function(matrix){  
+  sr <- length(unique(c(matrix)))
+  return(speciesRichness = sr)
+}
+
 #' @title Rank-abundance relation 
 #' @description Calculates the rank-abundance pattern for a community matrix. 
 #' @param matrix Integer matrix representing the species community
--- PhylGeo/R/speciesRichness.R
+++
@@ -1,8 +0,0 @@
-#' @title Species richness
-#' @description Calculate the total species richness of a community
-#' @param matrix A square matrix containing a species community (one individual per grid cell) 
-#' @return An integer value for the species richness
-specRich <- function(matrix){  
-  sr <- length(unique(c(matrix)))
-  return(speciesRichness = sr)
-}
