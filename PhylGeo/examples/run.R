e867d4ac4a3d5442dd6f72c01e050a65d6c8720c
Florian Hartig <florianhartig@users.noreply.github.com>
2015-08-28 18:03:06 +0200

    Updated example

--- PhylGeo/examples/run.R
+++ PhylGeo/examples/run.R
@@ -7,7 +7,7 @@ library(PhylGeo)
 # dispersal = 3
 # dispersal = 2 
 
-myModel <- fullMod(x = 50, y = 50, dispersal = 0.5, runs = 2000, specRate = 1.0, density = T, environment = T, seed = sample(1:1000,1), mortalityStrength = 2, saveLocation = "./")
+myModel <- fullMod(x = 100, y = 100, dispersal = 0.5, runs = 1000, specRate = 1.0, density = F, environment = F, seed = 1, fitnessBaseMortalityRatio = 5)
 
 # Look at the phylogeny (requires package 'ape')
 require(ape)
@@ -23,11 +23,13 @@ extantPhylogeny <- drop.fossil(phylogeny)
 
 #plot(extantPhylogeny)
 
+plotTraitDistribution(myModel)
 
 plotSpatialPhylo(myModel$specMat, extantPhylogeny, plot = "both")
 
 specRich(myModel$specMat)
 
-#par(mfrow = c(1,2))
-#rac(myModel$specMat)
-#sac(myModel$specMat)
+
+par(mfrow = c(1,2))
+rac(myModel$specMat)
+sac(myModel$specMat)
