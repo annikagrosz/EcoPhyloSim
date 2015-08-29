4eb3e9b88c750e91d9fc765debe9a1246193939c
Florian Hartig <florianhartig@users.noreply.github.com>
2015-08-29 12:16:32 +0200

    updates to plot spatial function

--- PhylGeo/R/plotSpatialPhylo.R
+++ PhylGeo/R/plotSpatialPhylo.R
@@ -14,13 +14,13 @@ plotSpatialPhylo <- function (simu, plot = "both", plotTraits = T, col = "phylod
   landscape = simu$specMat
   
   if (plotTraits == T){
-    library(adephylo)
-    library(shape)
+    #library(adephylo)
+    #library(shape)
     traits = getAverageTraits(simu)  
-    rootDist <- distRoot(extantPhylogeny, 1)
+    #rootDist <- distRoot(extantPhylogeny, 1)
   }
   
-  tipDistances <- as.vector(as.matrix(distTips(extantPhylogeny, 1:2))[,1])+1
+  tipDistances <- cumsum(c(1, diag(as.matrix(distTips(extantPhylogeny))[,-1])))
   
   if (col == "phylodist") cols<-rainbow(max(tipDistances), start = 0.19)[tipDistances]
   if (col == "equidist")  cols<-rainbow(nSpecies, start = 0.19)
@@ -34,19 +34,28 @@ plotSpatialPhylo <- function (simu, plot = "both", plotTraits = T, col = "phylod
 
   
   if (plotTraits == T){
-    layout(mat = matrix(1:3, nrow = 1), widths = c(1,0.3,1))
-    xval = seq(1.25 * rootDist, 1.5*rootDist, len = 4)
-    plot(extantPhylogeny, tip.color = cols, x.lim = 1.5*rootDist)
-    size =  dev.size()[1] / 6
+    layout(mat = matrix(1:6, nrow = 2, byrow = T), widths = c(1,0.5,1), heights = c(0.15,0.8))
+    par(mar = c(0,0,0,0), oma = c(2,2,2,2))
     
-    plot(rep(xval, each = nSpecies), rep(1:nSpecies, 4), cex = size*traits, pch = 16, frame = F, yaxt='n', xaxt='n', ann=FALSE)
+    plot.new()
+    plot(NULL,frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7), ylim = c(0,3))
+    text(1:4 - 0.5, 0, colnames(traits), srt = 45, pos = 4)
 
+    plot.new()
+    
+    plot(extantPhylogeny, tip.color = cols)
+
+
+    plot(rep(1:4, each = nSpecies), rep(1:nSpecies, 4), cex = size*traits, pch = 16, frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7))
+
+
+    
     }else{
     oldpar <- par(mfrow = c(1,3), mar = c(3,3,3,3))
     plot(extantPhylogeny, tip.color = cols) 
     par(oldpar)
   }
-  
+  par(mar = c(1,1,1,1))
   image(landscape, col = cols)
 
   
--- PhylGeo/R/traitsGetter.R
+++ PhylGeo/R/traitsGetter.R
@@ -10,14 +10,15 @@ getAverageTraits <- function(simu, orderPhylo = T){
   traits <- matrix(nrow = length(species), ncol = 4)
   
   rownames(traits) = species
-  colnames(traits) = c("trait", "environment", "competition", "neutral")
+  colnames(traits) = c("environment", "env trait", "comp trait", "neutral trait")
   
   for (i in 1:length(species)){
     sel = simu$specMat == species[i]
-    traits[i,1] = mean(simu$traitMat[sel])
-    traits[i,2] = mean(simu$envMat[sel])
+    traits[i,1] = mean(simu$envMat[sel])
+    traits[i,2] = mean(simu$traitMat[sel])
     traits[i,3] = mean(simu$compMat[sel])
     traits[i,4] = mean(simu$neutMat[sel])
+
   }
   return(traits)
 }
