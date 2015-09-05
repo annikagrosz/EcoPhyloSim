67eb12fcbbd2b2579521402e961b00762e163987
Florian Hartig <florian.hartig@gmail.com>
2015-09-05 22:55:44 +0200

    updates 100x100 runs

--- PhylGeo/examples/PhylogeneticDispersionConfigurations/run100x100.r
+++ PhylGeo/examples/PhylogeneticDispersionConfigurations/run100x100.r
@@ -2,10 +2,8 @@ require(PhylGeo)
 
 # SET WD TO SOURCE FILE LOCATION !!!!
 
-modes <- c(rep("global",4), rep("local",16))
-scenarios <- paste(modesDists, rep(c("Envi", "Neut", "Dens", "Hybr"),4), sep = "")
 
-runs = round(seq(250, 300, len = 10))
+runs = round(seq(25000, 50000, len = 50))
 
 dispOptions = 4
 fitOptions = 7
@@ -14,9 +12,13 @@ dispersal =  rep(c(0,0.5, 2,4), each = fitOptions)
 density = rep(c(0,seq(0,1,len = fitOptions-2),1),dispOptions)
 environment = rep(c(0,seq(1,0,len = fitOptions-2),1),dispOptions)
 
+modes <- ifelse(dispersal == 0, "global", "local")
+scenarios <- paste(modes, " dens=", density, " env=", environment, sep = "")
+
+
 pars <- list()
 
-for (i in 1:20){pars[[i]] = createCompletePar(x = 100,y = 100, runs = runs, modes = modes[i],scenario = scenarios[i], dispersal = dispersal[i],specRate = 1,density = density[i],environment = environment[i], fitnessBaseMortalityRatio = 5, densityCut = 1,seed = 1000)}
+for (i in 1:length(scenarios)){pars[[i]] = createCompletePar(x = 100,y = 100, runs = runs, modes = modes[i],scenario = scenarios[i], dispersal = dispersal[i],specRate = 1,density = density[i],environment = environment[i], fitnessBaseMortalityRatio = 5, densityCut = 1,seed = 1000)}
 
 
 system.time(simulationOut <- fullModBatch(pars, parallel = T)) 
