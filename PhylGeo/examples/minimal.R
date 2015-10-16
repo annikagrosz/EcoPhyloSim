77e9f2e9a3674c5565657e794173700f87faa104
Florian Hartig <florianhartig@users.noreply.github.com>
2015-10-16 13:32:08 +0200

    minor updates

---
+++ PhylGeo/examples/minimal.R
@@ -0,0 +1,10 @@
+
+library(PhylGeo)
+
+par <- createCompletePar(x = 50, y = 50, dispersal = 1, runs = c(500,1000), density = 1, environment = 0.5, specRate = 1)
+
+system.time(simu <- runSimulation(par))
+
+plotSpatialPhylo(simu, plot = "both")
+
+plotTraitDistribution(simu)
