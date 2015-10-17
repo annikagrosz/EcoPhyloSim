7d1b783f644c71cc3c13759666bc16c835de3070
stefan-paul <stefan_paul_1@web.de>
2015-10-17 14:50:25 +0200

    Added plotting options to plotSpatialPhylo.R; some changes in the Vignette.

--- PhylGeo/R/plotSpatialPhylo.R
+++ PhylGeo/R/plotSpatialPhylo.R
@@ -2,8 +2,11 @@
 #' @description Plots the phylogeny and species landscape, colored by taxon 
 #' @param simu simulation output, consisting of a list contain at least $specMat with the spatial species matrix, and $phylogeny with the phylogeny
 #' @param phylogeny the corresponding (extant) phylogeny
-#' @param plot defines what to plot. "both" plots the landscape and phylogeny side-by-side
-#' 
+#' @param plot defines what to plot. "both" plots the landscape and phylogeny side-by-side, other options are "landscape" and "phylogeny"
+#' @param plotTraits defines if the traits should be plotted
+#' @param time defines which simulation to choose in case your simu object contains multiple simulations. The default is the last one.
+#' @param col defines the color, options are "phylodist" (default) and "equidist" 
+#' @param main defines the title of the plot
 #' @import ape
 #' @importFrom adephylo distTips
 #' @export
@@ -37,36 +40,51 @@ plotSpatialPhylo <- function (simu, plot = "both", plotTraits = T, col = "phylod
   
   landscape <- matrix(as.numeric(factor(landscape, levels = tipNumbers, labels = 1:nSpecies)), nrow = nrow(landscape))
   
-  
-  
-  if (plotTraits == T){
-    layout(mat = matrix(1:6, nrow = 2, byrow = T), widths = c(1,0.5,1), heights = c(0.15,0.8))
-    par(mar = c(0,0,0,0), oma = c(2,2,2,2))
+  if(plot!="both"){
+    if(plotTraits==T){
+      layout(mat = matrix(1:4, nrow = 2, byrow = T), widths = c(0.5,1), heights = c(0.15,0.8))
+      par(mar = c(0,0,0,0), oma = c(2,2,2,2))
+      plot(NULL,frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7), ylim = c(0,3))
+      text(1:4 - 0.5, 0, colnames(traits), srt = 45, pos = 4)
+      plot.new()
+      size = dev.size()[2]/3
+      plot(rep(1:4, each = nSpecies), rep(1:nSpecies, 4), cex = size*traits, pch = 16, frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7))
+    }
     
-    plot.new()
-    plot(NULL,frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7), ylim = c(0,3))
-    text(1:4 - 0.5, 0, colnames(traits), srt = 45, pos = 4)
-    
-    plot.new()
-    size = dev.size()[2]/3
-    
-    plot(extantPhylogeny, tip.color = cols, cex = 1.3)
-    
-    
-    plot(rep(1:4, each = nSpecies), rep(1:nSpecies, 4), cex = size*traits, pch = 16, frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7))
-    
-    
-    
-  }else{
-    oldpar <- par(mfrow = c(1,3), mar = c(3,3,3,3))
-    plot(extantPhylogeny, tip.color = cols) 
-    par(oldpar)
+    if(plotTraits==F){
+      par(mfrow=c(1,1),mar = c(1,1,1,1))
+    }
+    if(plot=="landscape"){
+      image(landscape, col = cols, yaxt='n', xaxt='n')
+    }
+    else{
+      if(plot=="phylogeny"){
+        plot(extantPhylogeny, tip.color = cols, cex = 1.3)
+      }
+      else stop("wrong plot argument")
+    }
   }
-  par(mar = c(1,1,1,1))
-  image(landscape, col = cols, yaxt='n', xaxt='n')
-  
+  else{
+    if(plotTraits==T){
+      layout(mat = matrix(1:6, nrow = 2, byrow = T), widths = c(1,0.5,1), heights = c(0.15,0.8))
+      par(mar = c(0,0,0,0), oma = c(2,2,2,2))
+      plot.new()
+      plot(NULL,frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7), ylim = c(0,3))
+      text(1:4 - 0.5, 0, colnames(traits), srt = 45, pos = 4)
+      plot.new()
+      plot(extantPhylogeny, tip.color = cols, cex = 1.3)
+      size = dev.size()[2]/3
+      plot(rep(1:4, each = nSpecies), rep(1:nSpecies, 4), cex = size*traits, pch = 16, frame = F, yaxt='n', xaxt='n', ann=FALSE, xlim = c(0,7))
+      par(mar = c(1,1,1,1))
+      image(landscape, col = cols, yaxt='n', xaxt='n')
+    }
+    else{
+      par(mfrow=c(1,2))
+      plot(extantPhylogeny, tip.color = cols, cex = 1.3)
+      image(landscape, col = cols, yaxt='n', xaxt='n')
+    }
+  }
   mtext(main, outer = T)
   
 }
-
 
