11ea900518bc08acccc7fdc7019100342c811d16
christianbuschbeck <christian.buschbeck@gmail.com>
2015-10-21 08:49:37 +0200

    kleine tippfehler

--- PhylGeo/R/parametersXML.R
+++ PhylGeo/R/parametersXML.R
@@ -24,12 +24,12 @@ getParametersXML <- function(file){
 #' @param scenarios Character vector containing the names of the scenarios to be run
 #' @param x  Dimension of the model landscape in x-direction
 #' @param y  Dimension of the model landscape in y-direction
-#' @param dispersal 1 = Gloabl dispersal, 2 = Nearest neighbor dispersal 
+#' @param dispersal 1 = Global dispersal, 2 = Nearest neighbor dispersal 
 #' @param nSpec  Number of initial Species in the model landscape
-#' @param specRate Numerical the number of species introduced into the community during each generation
+#' @param specRate Numerical, the number of species introduced into the community during each generation
 #' @param runs  Number of generations the model runs over
-#' @param density Logical determining whether or not density dependence influences the model
-#' @param densityStrength Numerical (0.0 - 1.0) determining the strength of the density dependence
+#' @param density Logical, determining whether or not density dependence influences the model
+#' @param densityStrength Numerical, (0.0 - 1.0) determining the strength of the density dependence
 #' @param location A character vector specifing the folder to save the .xml file in (eg. "C:/parametersR/"). The "/" at the end is vital.
 #' @param fileName A character vector specifing the name of the xml file (eg. "myParameters.xml")
 #' @return Creates a .xml file at the specified location
@@ -76,19 +76,19 @@ setLeipzigParametersXML <- function(scenarios, x, y, runs, dispersal, nSpec, spe
 #' @param scenarios Character vector containing the names of the scenarios to be run
 #' @param x  Dimension of the model landscape in x-direction
 #' @param y  Dimension of the model landscape in y-direction
-#' @param dispersal 1 = Gloabl dispersal, 3 = Local dispersal 
-#' @param specRate Numerical the number of species introduced into the community during each generation
+#' @param dispersal 1 = Global dispersal, 3 = Local dispersal 
+#' @param specRate Numerical, the number of species introduced into the community during each generation
 #' @param runs  Number of generations the model runs over
-#' @param density Logical determining whether or not density dependence influences the model
-#' @param environment Logical determining whether or not the environment influences the model
+#' @param density Logical, determining whether or not density dependence influences the model
+#' @param environment Logical, determining whether or not the environment influences the model
 #' @param mortalityFitness Logical, determining whether or not the individual fitness influences mortality
-#' @param mortalityStrength Integer determining the influence of fitness on mortality
+#' @param mortalityStrength Integer, determining the influence of fitness on mortality
 #' @param reproductiveFitness Logical, determining whether or not the individual fitness influences reproducti
-#' @param neutral Logical determining whether or not the model is run under neutral conditions (overrides density and environment)
+#' @param neutral Logical, determining whether or not the model is run under neutral conditions (overrides density and environment)
 #' @param location A character vector specifing the folder to save the .xml file in (the filename is always parameters.xml and does not need to be set.)
-#' @param dispersalCut Integer defining the maximum dispersal distance of the kernel
-#' @param densityCut Integer defining the range for the density dependence
-#' @param seed Integer setting the random seed for the model
+#' @param dispersalCut Integer, defining the maximum dispersal distance of the kernel
+#' @param densityCut Integer, defining the range for the density dependence
+#' @param seed Integer, setting the random seed for the model
 #' @param saveLocation Path of the folder to save the extracted files to
 #' @return Creates a .xml file at the specified location
 setModelParametersXML <- function(list, file)
