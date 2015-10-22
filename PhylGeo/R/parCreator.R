df0f637e077e0341c3b3a20aec62432b8df0b96a
stefan-paul <stefan_paul_1@web.de>
2015-10-22 21:04:08 +0200

    Changes in the Documentation

--- PhylGeo/R/parCreator.R
+++ PhylGeo/R/parCreator.R
@@ -1,21 +1,21 @@
 #' @title  Parameter Generator 
-#' @description Function to create a list with parameters for biogeographical simulations with \code{\link[PhylGeo]{runSimulation}} or \code{\link[PhylGeo]{runSimulationBatch}} 
+#' @description Function to create a list with parameters for biogeographical simulations with \code{runSimulation} or \code{runSimulationBatch}
 #' @param x  Integer, Dimension of the model landscape in x-direction
 #' @param y  Integer, Dimension of the model landscape in y-direction
-#' @param dispersal Integer. Type 0 or "global" for global dispersion. For local dispersion all integers >=1 are possible. A higer the value represents a larger dispersal distance.
-#' @param runs  Integer, Number of generations the model runs over. You can also define a sequence of runs. Then the intermediate and end results are saved.
+#' @param dispersal Integer. Type 0 or "global" for global dispersion. For local dispersion all integers >=1 set the dispersal distance.
+#' @param runs  Integer, Number of generations or sequence of generations the model runs overn (see Details). 
 #' @param specRate Integer, Number of Individuals introduced to the community in each generation
-#' @param density Logical, determining whether or not density dependence influences the model
-#' @param environment Logical, determining whether or not the environment influences the model
+#' @param density Float, determining whether or how strong the density dependence influences the model. By default (density=0) there is no density dependence. The higher the value of the parameter, the stronger is the density dependence.
+#' @param environment Float, determining whether or how strong the environment influences the model.  By default (environment=0) there is no influence of the environment. The higher the value of the parameter, the stronger is the influence of the environment.
 #' @param fitnessActsOn Character, determining how the fitness influences the individuals. Possible inputs are "mortality" (default), "reproduction" or "both"
 #' @param fitnessBaseMortalityRatio Integer, TODO
 #' @param densityCut Integer, defines the effective range of the competition (ignored if density = FALSE)
 #' @param seed numerical, sets the random seed
 #' @param type Character, determining which model should be used. "base" is running the default model. Other possibilities are "Leipzig" and "Rneutral" which will run a neutral model purely in R.
-#' @details If type = "Rneutral" the model will run entirely in R. This model is to be seen only for test and teaching purpose. To be used in practice it is far too slow. Also the output is reduced in comparision with the other models. Only the species landscape and the parameter settings will be displayed in the output.
+#' @details If runs is a sequence of generations the intermediate and end results are saved in the output of \code{runSimulation}. \cr\cr If type = "Rneutral" the model will run entirely in R. This model is to be seen only for test and teaching purpose. To be used in practice it is far too slow. Also the output is reduced in comparision with the other models. Only the species landscape and the parameter settings will be displayed in the output.
 #' @return A List with parameters
 
-createCompletePar <- function(x = 50, y = 50, dispersal = "global", runs = 100, specRate = 1.0, density = F, environment = F, fitnessActsOn = "mortality" , fitnessBaseMortalityRatio = 10, densityCut = 1, seed=NULL,  type = "base", modes = NULL, scenario = NULL){
+createCompletePar <- function(x = 50, y = 50, dispersal = "global", runs = 100, specRate = 1.0, density = 0, environment = 0, fitnessActsOn = "mortality" , fitnessBaseMortalityRatio = 10, densityCut = 1, seed=NULL,  type = "base", modes = NULL, scenario = NULL){
     
   
   if (length(runs)>1){
--- PhylGeo/R/runSimulationBatch.R
+++ PhylGeo/R/runSimulationBatch.R
@@ -1,17 +1,34 @@
 
 #' @title Batch runner
-#' @description A model of species community assembly under different assembly mechanisms, using parallel computing to make use of multi core cpus and clusters in order to reduce computation time.
-#' @param pars A single set of parameters as created by \code{\link[PhylGeo]{createCompletePar} or a list of parameter sets
-#' @param parallel The number of cores to be used for parallel computing (default null is no parallelization)
+#' @description A model of species community assembly under different assembly mechanisms, using parallel computing to make use of multi core cpus and clusters in order to reduce computation time. The function is an extension to \link{runSimulation} in order to simulate multiple scenarios.
+#' @param pars A single set of parameters as created by \code{\link{createCompletePar}} or a list of parameter sets
+#' @param parallel Logical, determining whether parallel computing should be executed.
 #' @param backup Logical determining whether the results of the individual scenario runs should be saved as a workspace image (advised if the simulation takes a long time, or if the individual scenarios vary greatly in runtime. Default is FALSE)
-#' @return A list containing numerical matrices of species distribution, local trait values and the environment for each scenario (each cell in a matrix represents an individual of a species), plus a phylogeny object of class "phylo" for each scenario.
+#' @return A list of Phylosim-Objects as created by \code{\link{runSimulation}}
 #' @details This function uses the \code{\link{foreach}} and \code{\link{doParallel}} package to compute the model scenarios parallel on several cores. If you want to keep working on your computer make sure to reserve at least one core for your other endevors (by assigning n-1 cores to the function). By default all cores are employed to ensure maximum speed.\cr \cr The phylogeny is passed to R in the newick format and parsed to an object of class "phylo" with the function \code{\link[ape]{read.tree}} from the \code{\link{ape}} package. 
+#' @examples 
+#' ## Define two parameter sets
+#' par1 <- createCompletePar(x = 50, y = 50, dispersal = 0 , runs = c(500,1000),
+#'         density = 0, environment = 0.5, specRate = 1) 
+#' par2 <- createCompletePar(x = 50, y = 50, dispersal = 5 , runs = c(500,1000),
+#'         density = 1, environment = 0.5, specRate = 1)
+#' 
+#' ## Merge the parameter sets. It is important to note, that the funktion
+#' ## needs a list of parameter sets.
+#' par <- list(par1,par2)
+#' 
+#' ## Run the model
+#' simu <- runSimulationBatch(par, parallel=T) 
+#'
+#' ## Compare the results
+#' ##
+
 #' @export
 runSimulationBatch <- function(pars, parallel = F, backup = FALSE){
   #start timing
   ptm <- proc.time() 
   
-  # getParametersXML(XMLfile)
+  # TODO getParametersXML(XMLfile)
   
   if (parallel != F){
     cat("running ",length(pars), " batch simualations with parallelization", "\n")
