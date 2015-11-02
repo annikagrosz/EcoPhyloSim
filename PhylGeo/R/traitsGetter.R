1e96d86777074c9d9d5aff9cb31b5c5293f73ef4
christianbuschbeck <christian.buschbeck@gmail.com>
2015-11-02 21:07:01 +0100

    documentation

--- PhylGeo/R/traitsGetter.R
+++ PhylGeo/R/traitsGetter.R
@@ -1,4 +1,15 @@
-getAverageTraits <- function(simu, orderPhylo = T){
+#' @title Get Average Traits
+#' @description calculates the arithmetic mean of the trait values for each species. 
+#' @param simu Simulation output of the class "Phylosim", usually consisting out of several lists. Needs to contain at least the spatial species matrix ($specMat) the environment matrix ($envMat) and the three different trait matrizes ($traitMat,$compMat, $neutMat)    
+#' @param which.simulation defines which simulation run to choose in case you defined to save at multiple time steps. The default is the last one.
+#' @param orderPhylo defines if the mean traits shall be calculated for extinct species as well. default is yes (TRUE) 
+#' @import 
+#' @export
+
+getAverageTraits <- function(simu,which.simulation =NULL,orderPhylo = T){
+  
+  if (is.null(which.simulation)) which.simulation = length(simu) - 1
+  simu <- simu[[which.simulation]]
   
   if(orderPhylo == T){
     extantPhylogeny <- drop.fossil(simu$phylogeny)
