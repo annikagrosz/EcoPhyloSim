dab99514e86a427b17a91f115d83551bc050b137
christianbuschbeck <christian.buschbeck@gmail.com>
2015-11-06 12:24:47 +0100

    .

--- PhylGeo/R/plotTraitDistribution.R
+++ PhylGeo/R/plotTraitDistribution.R
@@ -41,11 +41,7 @@ plotTraitDistribution <- function (simu, type = "hist", which.simulation = NULL)
   red <- sample(p, size = length(names))
   green <- sample(p, size = length(names))
   blue <- sample(p, size = length(names))
-<<<<<<< HEAD
-  cols <- numeric(length(names))
-=======
   cols <- vector()
->>>>>>> bca7606fb87b7fe6109b4e2d4feb4a76781aa0b3
   for(i in 1:length(names)){
       cols[i] <- rgb(red[i],green[i],blue[i],0.5)}
   colmat <- dat$specMat
@@ -71,7 +67,7 @@ plotTraitDistribution <- function (simu, type = "hist", which.simulation = NULL)
   
     
    
-    
+ 
     ymax <- max(hist(dat$traitMat,breaks=100, plot =F)$counts)
     xmax <- max(dat$traitMat) 
     xmin <- min(dat$traitMat)
