2d9f137a4b91ebba891a7cf8592b9176a35d31e8
christianbuschbeck <christian.buschbeck@gmail.com>
2015-11-11 12:00:21 +0100

    main package help file

--- help/addRtoolsToPath.R
+++ help/addRtoolsToPath.R
@@ -1,19 +1,21 @@
 # check if RTools really not there
 Sys.getenv()['PATH']
-
-# Add Rtools to path
-
 Sys.setenv('PATH' = paste( "c:\\\\Rtools\\\\bin;c:\\\\Rtools\\\\gcc-4.6.3\\\\bin;",  Sys.getenv('PATH'), sep =""))
 
 
 # Path for pdflatex
+Sys.getenv()['PATH']
+Sys.setenv(PATH = paste(Sys.getenv("PATH"), "C:\\Users\\de.student\\Desktop\\NEW\\miktex\\bin\\" , sep=.Platform$path.sep))
 
 Sys.setenv(PATH = paste(Sys.getenv("PATH"), "D:\\Uni\\Hiwi\\TEX\\miktex\\bin\\" , sep=.Platform$path.sep))
 #simulation----------------
 
+#C:\Users\de.student\Desktop\NEW\miktex\bin
 
 par <- createCompletePar(x = 50, y = 50, dispersal = 1, runs = c(500,1000), density = 1, environment = 0.5, specRate = 1)
 
 system.time(simu <- runSimulation(par))
 
 simu <- runSimulationBatch(par)
+
+install.packages("knitr")
