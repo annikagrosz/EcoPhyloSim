7cb0c551707d9d83a98f9fc9e744e99d63e2e119
stefan-paul <stefan_paul_1@web.de>
2016-01-22 19:53:24 +0100

    Changed size argument in localPlots and added possibility to generate nested plots.

--- PhyloSim/R/localPlots.R
+++ PhyloSim/R/localPlots.R
@@ -2,16 +2,18 @@
 #' @description Generates subplots from a metacommunity, simulating field conditions
 #' @param simu Simulation output of the class "Phylosim", usually consisting out of several lists. Needs to contain at least a species matrix ($specMat)
 #' @param which.result Integer, determines which result should be used. This argument is only usefull if your 'runs' argument in \code{\link{createCompletePar}} contains more than one element. By default (NULL), the last result is used.
-#' @param size A single value or a vector determining the size(s) of the generated subplots
+#' @param size A single value (or a vector) determining the edge length(s) of the generated subplots
 #' @param n The number of subplots to be generated
 #' @param community Logical, determining whether to generate a communiy table or not. default is FALSE
 #' @param plot Logical, determining whether the results should be shown graphically. Default is FALSE.
+#' @param nested Logical, determinig whether the subplots should be nested (see Details).
 #' @return A list of subplots and (if coomunity = T) a community table with plots in rows and species in collumns
+#' @details If nested == TRUE the subplots are poduced in a sequential order. That means if your 'size' argument has the length 5 and your 'n' argument has the size 2 you will get ten subplots. The first five will be one group of nested plots and the last five the second group of nested plots.
 #' @export
-localPlots <- function(simu,which.result=NULL,size, n, community = FALSE, plot = FALSE){
+localPlots <- function(simu,which.result=NULL,size, n, community = FALSE, plot = FALSE, nested = FALSE){
   
   if(n ==1){
-    n <- 2 
+   # n <- 2 
     single = T
   }else{
     single=F
@@ -25,51 +27,65 @@ localPlots <- function(simu,which.result=NULL,size, n, community = FALSE, plot =
   env <- simu$envMat
   if(plot == T) plotmat <-matrix(0, ncol(matrix), nrow(matrix))
   
-  edge <- round(sqrt(size))-1
+ 
   subPlots <- list()
   envPlots <- list()
   communityTable <- data.frame("species"= numeric())
+  count <-0
   
   for(i in 1:n)
-  {                                                                          
+  {           
+    if(nested == TRUE){
+      x = sample(x=1:length(matrix[1,]),size=1)
+      y = sample(x=1:length(matrix[,1]),size=1)
+    }
+    
+    for(k in 1:length(size)){
+    count <- count + 1
+    edge <- size[k] 
+    
+    if(nested == FALSE){
     x = sample(x=1:length(matrix[1,]),size=1)
-    y = sample(x=1:length(matrix[,1]),size=1)
+    y = sample(x=1:length(matrix[,1]),size=1)      
+    }
     
     # Check for boundary issues and implemant warped bounaries if necessary
-    if(x+edge > length(matrix[1,]) && y+edge <= length(matrix[,1])){
+    if(x+(edge-1) > length(matrix[1,]) && y+(edge-1) <= length(matrix[,1])){
       rsel = c(seq(x,length(matrix[1,])), seq(1,(edge-length(seq(x,length(matrix[1,]))))))
-      csel = seq(y,y+edge)
+      csel = seq(y,y+(edge-1))
     }
-    else if(x+edge <= length(matrix[1,]) && y+edge > length(matrix[,1])){
-      rsel = seq(x,x+edge)
+    else if(x+(edge-1) <= length(matrix[1,]) && y+(edge-1) > length(matrix[,1])){
+      rsel = seq(x,x+(edge-1)) 
       csel = c(seq(y,length(matrix[,1])),seq(1,(edge-length(seq(y,length(matrix[,1]))))))
     }
-    else if(x+edge > length(matrix[1,]) && y+edge > length(matrix[,1])){
+    else if(x+(edge-1) > length(matrix[1,]) && y+(edge-1) > length(matrix[,1])){
       rsel = c(seq(x,length(matrix[1,])), seq(1,(edge-length(seq(x,length(matrix[1,]))))))
       csel = c(seq(y,length(matrix[,1])), seq(1,(edge-length(seq(y,length(matrix[,1]))))))
     }
     else{
-      rsel = seq(x,x+edge)
-      csel = seq(y,y+edge)
+      rsel = seq(x,x+(edge-1))
+      csel = seq(y,y+(edge-1))
     }
     
     
-    subPlots[[i]] <- matrix[rsel, csel]
-    envPlots[[i]] <- env[rsel, csel]    
+    subPlots[[count]] <- matrix[rsel, csel]
+    envPlots[[count]] <- env[rsel, csel]    
     
     if(community == T)
     {
-      dataTable <- as.data.frame(table(subPlot))
-      names(dataTable) <- c("species", paste("plot", i, collapse="", sep="")) 
-      communityTable <- merge(communityTable, dataTable , all=T)
-      communityTable[is.na(communityTable)] <- 0
+      dataTable <- as.data.frame(table(subPlots))
+      names(dataTable) <- c("species", paste("plot", count, collapse="", sep="")) 
+      communityTable <- merge(communityTable, dataTable , all=TRUE, by="species")
+      #communityTable[is.na(communityTable)] <- 0
     } 
+    
     if(plot == T){
       plotmat[rsel, csel] <- 1
   
     }
-    
+    }
    }
+  
   
   speciesNames <- character()
   for(b in 1:length(communityTable$species))
--- PhyloSim/R/plotSAC.R
+++ PhyloSim/R/plotSAC.R
@@ -3,9 +3,10 @@
 #' @description Plots the species area curve for a given community. 
 #' @param simu Simulation output of the class "Phylosim", usually consisting out of several lists. Needs at least the spatial distribution of the species stored in a matrix ($specMat)
 #' @param which.result Integer, determines which result should be used. This argument is only usefull if your 'runs' argument in \code{\link{createCompletePar}} contains more than one element. By default (NULL), the last result is used. If you choose "all" all results are shown in one plot (see 'Details).
-#' @param area a single value or a vector determining the size(s) of the subplots. If not provided, 10 logarithmic plotsizes will be used.
+#' @param size a single value or a vector determining the edge length of the subplots. If not provided, 10 logarithmic plotsizes will be used.
 #' @param rep The number of repetitions per size to calculate the mean 
 #' @param plot Logical determining whether to plot the SAC or not
+#' @param nested Logical, determining whether nested subplots should be used.
 #' @param title String, determining the title of the plot
 #' @details Displays the accumulated species richness as a function of plot size or the amount of equally sized plots. It serves as an indicator for the clustering of a species community. A positively bent curve usually indicates clustering since an increase in plot size or number leads to an increase in species richness while a negatively bent curve indicates a more neutral distribution of species within the community. \cr\cr If which.result = "all" all intermediate results are shown in one plot. The colors of the lines are plotted as a gradient from blue (first results) to red (end result).
 #' @return A list containing the mean species richness for each size and the respective standard deviation. If which.result = "all" only the plot will be returned.
@@ -38,7 +39,7 @@
 #'  
 #' #Plot SAC curve with random plotsize 
 #' 
-#' sac(simu=simu.envcom.local, area = sort(sample(c(10:1000), size = 10)))
+#' sac(simu=simu.envcom.local, size = sort(sample(c(10:1000), size = 10)))
 #' 
 #'  
 #'  
@@ -52,7 +53,7 @@
 #' @export
 #' 
 ## Modification of the SAC function to plot all recorded time steps
-sac <- function(simu, which.result = NULL, area = NULL, rep=50, plot=T, title="SAC"){
+sac <- function(simu, which.result = NULL, size = NULL, rep=50, plot=T, title="SAC" , nested = FALSE){
   
   meanSpeciesRichnessUpperCI <- numeric()
   meanSpeciesRichnessLowerCI <- numeric()
@@ -80,64 +81,71 @@ sac <- function(simu, which.result = NULL, area = NULL, rep=50, plot=T, title="S
     
     landscapeDim = dim(matrix)
     landscapeArea = landscapeDim[1] * landscapeDim[2]
-    if (is.null(area)){
-      area = exp(seq(1, log(landscapeArea),len = 10))
+    if (is.null(size)){
+      size = exp(seq(2, log(landscapeArea),len = 10))
     }
     
-    area = round(sqrt(area))^2
     
-    meanSpeciesRichness <- numeric()
-    
-    #Loop over all plot sizes
-    for(i in 1:length(area))
-    { 
-      # Repetitions for each plot size
+     meanSpeciesRichness <- numeric()
 
-      subPlots <- localPlots(size=area[i], n = rep, simu=simu, which.result = t)$subPlots
+      subPlots <- localPlots(size=size, n = rep, simu=simu, which.result = t, nested = nested)$subPlots
 
       speciesRichness <- sapply(subPlots, SR)
-      meanSpeciesRichness[i] <- mean(speciesRichness)
+      speciesRichnesslist <-list()
+      
+      f <- rep(1:length(size), length(speciesRichness)/length(size))
+      
+      for(i in 1: length(speciesRichness)){
+        if(i <= length(size)){
+          speciesRichnesslist[[f[i]]] <- speciesRichness[i]
+        }
+        else speciesRichnesslist[[f[i]]] <- c(speciesRichnesslist[[f[i]]],speciesRichness[i])
+      }
+      
+      for(i in 1:length(speciesRichnesslist)){
+        meanSpeciesRichness[i] <- mean(speciesRichnesslist[[i]])
+      
       
       if(t == 1){
-        meanSpeciesRichnessUpperCI[i] <- quantile(speciesRichness, probs = 0.95, na.rm = T)
-        meanSpeciesRichnessLowerCI[i] <- quantile(speciesRichness, probs = 0.05, na.rm = T)
+        meanSpeciesRichnessUpperCI[i] <- quantile(speciesRichnesslist[[i]], probs = 0.95, na.rm = T)
+        meanSpeciesRichnessLowerCI[i] <- quantile(speciesRichnesslist[[i]], probs = 0.05, na.rm = T)
       } else{
-        if(meanSpeciesRichnessUpperCI[i]< quantile(speciesRichness, probs = 0.95, na.rm = T)){
-           meanSpeciesRichnessUpperCI[i] <- quantile(speciesRichness, probs = 0.95, na.rm = T)
+        if(meanSpeciesRichnessUpperCI[i]< quantile(speciesRichnesslist[[i]], probs = 0.95, na.rm = T)){
+           meanSpeciesRichnessUpperCI[i] <- quantile(speciesRichnesslist[[i]], probs = 0.95, na.rm = T)
         }
-        if(meanSpeciesRichnessLowerCI[i] > quantile(speciesRichness, probs = 0.05, na.rm = T)){
-           meanSpeciesRichnessLowerCI[i] <- quantile(speciesRichness, probs = 0.05, na.rm = T)
+        if(meanSpeciesRichnessLowerCI[i] > quantile(speciesRichnesslist[[i]], probs = 0.05, na.rm = T)){
+           meanSpeciesRichnessLowerCI[i] <- quantile(speciesRichnesslist[[i]], probs = 0.05, na.rm = T)
         }
-      }
-    }
+      }}
     
     if(plot == T){
       if(t == 1){
         if(length(simulations)==1){
-          plot(area, meanSpeciesRichness, log="xy", xlab="Area (n cells)",
+          plot(size^2, meanSpeciesRichness, log="xy", xlab="Area (n cells)",
                ylab="Number of Species", main=title, col=t, lwd=2, pch=4, type="b")
           
-          polygon(x=c(area,rev(area)), y=c(meanSpeciesRichnessUpperCI,
+          polygon(x=c(size^2,rev(size^2)), y=c(meanSpeciesRichnessUpperCI,
                   rev(meanSpeciesRichnessLowerCI)), col="#00000030", border=NA)
           
-          lines(area, (meanSpeciesRichnessUpperCI), col="red", lty=2, lwd=2)
-          lines(area, (meanSpeciesRichnessLowerCI), col="red", lty=2, lwd=2)
+          lines(size^2, (meanSpeciesRichnessUpperCI), col="red", lty=2, lwd=2)
+          lines(size^2, (meanSpeciesRichnessLowerCI), col="red", lty=2, lwd=2)
           
-        }else  plot(area,meanSpeciesRichness,type="l", log="xy", xlab="Area (n cells)", ylab="Number of Species", main=title, col=cols[t])
+        }else  plot(size^2,meanSpeciesRichness,type="l", log="xy", xlab="Area (n cells)", ylab="Number of Species", main=title, col=cols[t])
       } else{
-        lines(area,meanSpeciesRichness,type="l", col=cols[t])
+        lines(size^2,meanSpeciesRichness,type="l", col=cols[t])
         
         if(t == length(simu$Output)){
-          polygon(x=c(area,rev(area)), y=c(meanSpeciesRichnessUpperCI,
+         
+          polygon(x=c(size^2,rev(size^2)), y=c(meanSpeciesRichnessUpperCI,
                   rev(meanSpeciesRichnessLowerCI)), col="#00000030", border=NA)
           
-          lines(area, (meanSpeciesRichnessUpperCI), col="red", lty=2, lwd=2)
-          lines(area, (meanSpeciesRichnessLowerCI), col="red", lty=2, lwd=2)
+          lines(size^2, (meanSpeciesRichnessUpperCI), col="red", lty=2, lwd=2)
+          lines(size^2, (meanSpeciesRichnessLowerCI), col="red", lty=2, lwd=2)
         }
       }
     }
   }
-  if(length(simulations)==1) return(data.frame(area = area, sr.Mean = meanSpeciesRichness,
+  if(length(simulations)==1) return(data.frame(size = size^2, sr.Mean = meanSpeciesRichness,
                                     sr.UpperCI = meanSpeciesRichnessUpperCI, sr.LowerCI = meanSpeciesRichnessLowerCI))
 }
 
