680170491eb8569aa368bf7a9f902357be05fc64
TankredO <Tankred_ott@hotmail.de>
2017-05-15 15:59:45 +0200

    Added further summary statistics

--- PhyloSim/R/summaryStatistics.R
+++ PhyloSim/R/summaryStatistics.R
@@ -18,8 +18,11 @@ calculateSummaryStatistics <- function(simulation) {
                             sacAuc=NA,
                             alphaDiversity = NA,
                             betaDiversity = NA,
+                            gammaDiversity = NA,
                             imbalance = NA,
-                            dispersion = NA)
+                            dispersion = NA,
+                            gammaStatistics = NA,
+                            meanNodeAge = NA)
   
   # TODO: implement special case: only 1 species
   if (is.double(simulation$Output[[1]]$phylogeny)) {
@@ -62,6 +65,8 @@ calculateSummaryStatistics <- function(simulation) {
   summaryStatistics$betaDiversity <- mean(vegan::betadiver(plots$communityTable,
                                                            method=method))
   
+  # gamma diversity
+  summaryStatistics$gammaDiversity <- gammaDiversity(simulation)
   
   # phylogenetic imbalance
   # uses apTreeshape's colless function
@@ -103,13 +108,44 @@ calculateSummaryStatistics <- function(simulation) {
   # phylogenetic dispersion
   # uses PhyloMeasures' implementation of the NRI
   
-  # Causes error. Drop fossil seems to drop clades that are no real fossils?!
+  # Commented code below causes error. Drop fossil seems to drop clades that are no real fossils?!
   #summaryStatistics$dispersion <- mean(PhyloMeasures::mpd.query(ape::drop.fossil(simulation$Output[[1]]$phylogeny),
   #                                                              plots$communityTable,
   #                                                              standardize = TRUE))
+  
   summaryStatistics$dispersion <- mean(PhyloMeasures::mpd.query(simulation$Output[[1]]$phylogeny,
                                                                 plots$communityTable,
                                                                 standardize = TRUE))
   
+  # gamma statistics
+  # uses ape's implementation of the gammaStatistics
+  # attention: only works on ultrametric trees
+  summaryStatistics$gammaStatistics <- ape::gammaStat(simulation$Output[[1]]$phylogeny)
+  
+  # mean node age
+  #summaryStatistics$meanNodeAge <- ape::chronoMPL(simulation$Output[[1]]$phylogeny)
+  summaryStatistics$meanNodeAge <- mean(simulation$Output[[1]]$phylogeny$edge.length)
+  
   return(summaryStatistics)
+}
+
+gammaDiversity <- function(simulation, q=0){
+  spec_vector <- c(simulation$Output[[1]]$specMat)
+  specs <- unique(spec_vector)
+  n_specs <- length(specs)
+  
+  if (q == 0) return(n_specs)
+  
+  n_individuals <- length(spec_vector)
+  
+  prop_abundances <- rep(NA, length(specs))
+  
+  for(i in 1:length(specs)){
+    prop_abundances[i] <- sum(spec_vector == specs[i]) / n_individuals
+  }
+  
+  gamma <- prop_abundances * prop_abundances^(q-1)
+  gamma <- sum(gamma)^(1/(q-1))
+  gamma <- 1/gamma
+  return(gamma)
 }
